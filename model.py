468cad111fbc4a33f90beea2ba79ecba719f1b39
Akshay Attaluri <akshay_a@mit.edu>
2025-10-30 23:40:48 -0700

    Simple decoder-only transformer implementation

---
+++ model.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dataclasses import dataclass
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.manual_seed(0)
+torch.set_default_device(device)
+
+
+@dataclass
+class Config():
+    seq_len = 8
+    batch_size = 32
+    d_model = 512
+    d_k = 64
+    d_v = 64
+    n_heads = 8
+    n_layers = 6
+    d_ff = 2048
+    vocab_size = 50257
+    learning_rate = 3e-4
+    bias = False
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, d_model, d_k, d_v, seq_len):
+        super().__init__()
+        # note that pretty much always d_k = d_v = d_model/n_heads (and so d_model should always be divisible by n_heads)
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.seq_len = seq_len
+        self.w_q = nn.Linear(d_model, d_k*n_heads, bias=False)
+        self.w_k = nn.Linear(d_model, d_k*n_heads, bias=False)
+        self.w_v = nn.Linear(d_model, d_v*n_heads, bias=False)
+        self.w_o = nn.Linear(n_heads * d_v, d_model)
+        self.register_buffer('mask', torch.triu(torch.ones(seq_len, seq_len, dtype=torch.bool), diagonal=1), persistent=False)
+
+    def forward(self, x):
+        # x is (b, t, d_model)
+        b, t, d_model = x.shape
+        Q, K, V = self.w_q(x), self.w_k(x), self.w_v(x)  # (b, t, n_heads*d_k), (b, t, n_heads*d_k), (b, t, n_heads*d_v)
+        Q = Q.view(b, t, self.n_heads, self.d_k).transpose(-2, -3)  # (b, n_heads, t, d_k)
+        K = Q.view(b, t, self.n_heads, self.d_k).transpose(-2, -3)  # (b, n_heads, t, d_k)
+        V = Q.view(b, t, self.n_heads, self.d_v).transpose(-2, -3)  # (b, n_heads, t, d_v)
+        masked_attention = (Q @ K.transpose(-2, -1)).masked_fill_(self.mask, -float('inf'))  # (b, n_heads, t, t)
+        intermediate = F.softmax(masked_attention / self.d_k**0.5, dim=-1)
+        scores = intermediate @ V  # (b, n_heads, t, d_v)
+        return self.w_o(scores.transpose(1, 2).contiguous().view(b, t, self.n_heads*self.d_v))  # (b, t, n_heads*d_v) @ (n_heads*d_v, d_model) = (b, t, d_model)
+
+
+class MLP(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.gelu = nn.GELU()
+        self.fc2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.fc2(self.gelu(self.fc1(x)))
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+        self.affine = nn.Parameter(torch.ones(d_model))
+        self.bias = nn.Parameter(torch.zeros(d_model))
+
+    def forward(self, x):  # (b, t, d_model)
+        mean = x.mean(dim=-1, keepdims=True)  # b, t
+        std = x.std(dim=-1, keepdims=True)  # b, t
+        ret = (x - mean) * self.affine / (std + self.eps) + self.bias
+        return ret
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_model, d_k, d_v, n_heads, d_ff, seq_len):
+        super().__init__()
+        self.ln1 = LayerNorm(d_model)
+        self.mha = MultiHeadAttention(n_heads, d_model, d_k, d_v, seq_len)
+        self.ln2 = LayerNorm(d_model)
+        self.mlp = MLP(d_model, d_ff)
+
+    def forward(self, x):
+        x = x + self.mha(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+        return x
+
+
+class TransformerModel(nn.Module):
+    def __init__(self, d_model, d_k, d_v, n_heads, d_ff, seq_len, n_layers, vocab_size):
+        super().__init__()
+        self.wte = nn.Embedding(vocab_size, d_model, device=device)
+        self.wpe = nn.Embedding(seq_len, d_model, device=device)
+        self.blocks = [TransformerBlock(d_model, d_k, d_v, n_heads, d_ff, seq_len) for _ in range(n_layers)]
+        self.lm_head = nn.Linear(d_model, vocab_size)
+        self.apply(self._init_weights)
+
+    def forward(self, x, targets):
+        b, t = x.shape
+
+        pos = torch.arange(0, t, dtype=torch.long, device=device)
+        x = self.wte(x) + self.wpe(pos)
+        for block in self.blocks:
+            x = block(x)
+        y = self.lm_head(x)  # 4, 8, 50000
+        loss = F.cross_entropy(y.transpose(-2, -1), targets)
+        return y, loss
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
---
+++ train.py
@@ -0,0 +1,76 @@
+from model import Config, TransformerModel
+import torch
+import tiktoken
+from datasets import load_dataset
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def preprocess_data():
+    ds = load_dataset("wikitext", "wikitext-2-raw-v1")
+
+    def join_records(split):
+        ret = ""
+        for elem in ds[split]['text']:
+            ret += elem + "\n\n"
+        return ret
+
+    raw_train = join_records("train")[0:10000]
+    raw_val = join_records("validation")[0:10000]
+
+    # vocab size of 50257
+    enc = tiktoken.get_encoding("gpt2")
+
+    # encode_ordinary basically excludes special tokens
+    train_ids = enc.encode(raw_train)
+    val_ids = enc.encode(raw_val)
+    return train_ids, val_ids
+
+
+def get_data(ids, cfg):
+    x = []
+    y = []
+
+    for id in range(0, len(ids) - 1 - cfg.seq_len * cfg.batch_size, cfg.batch_size * cfg.seq_len):
+        x_batch = []
+        y_batch = []
+        for batch in range(cfg.batch_size):
+            x_batch.append(ids[id+batch*cfg.seq_len:id+batch*cfg.seq_len+cfg.seq_len])
+            y_batch.append(ids[id+batch*cfg.seq_len+1:id+batch*cfg.seq_len+cfg.seq_len+1])
+        x.append(torch.tensor(x_batch, dtype=torch.long).to(device))
+        y.append(torch.tensor(y_batch, dtype=torch.long).to(device))
+
+    return (x, y)
+
+
+def main():
+    cfg = Config()
+    train_ids, val_ids = preprocess_data()
+    train_ids, val_ids = preprocess_data()
+    train_data = get_data(train_ids, cfg)
+    val_data = get_data(val_ids, cfg)
+    model = TransformerModel(cfg.d_model, cfg.d_k, cfg.d_v, cfg.n_heads, cfg.d_ff, cfg.seq_len, cfg.n_layers, cfg.vocab_size)
+    optim = torch.optim.AdamW(model.parameters(), lr=cfg.learning_rate)
+    n_steps = 10000
+    from tqdm import tqdm
+    model.train()
+    for step in tqdm(range(n_steps)):
+        l = 0
+        for batch in range(len(train_data)):
+            optim.zero_grad()
+            logits, loss = model(train_data[batch][0], train_data[batch][1])
+            l = loss
+            loss.backward()
+            optim.step()
+        if step % 500 == 0:
+            print(f"loss: {l}")
+    # eval
+
+    model.eval()
+    l = 0
+    with torch.no_grad():
+        for batch in range(len(val_data)):
+            logits, loss = model(val_data[batch][0], val_data[batch][1])
+            l += loss
+
+    print(f"val loss: {l/len(val_data)}")
